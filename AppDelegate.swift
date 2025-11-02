63172f08ace4835cb9af28ad5f714030b1719d52
emelehova <103140191+emelehova@users.noreply.github.com>
2025-11-03 00:35:27 +0300

    Refactor AppDelegate for improved window setup

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,51 +1,40 @@
 import Cocoa
+import WebKit
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
-    var browserVC: BrowserViewController!
+    var controller: BrowserViewController!
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let args = CommandLine.arguments
-        browserVC = BrowserViewController(arguments: args)
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let screen = NSScreen.main?.visibleFrame ?? NSRect(x: 0, y: 0, width: 1200, height: 800)
+        let width: CGFloat = 1200
+        let height: CGFloat = 800
+        let rect = NSRect(
+            x: screen.midX - width/2,
+            y: screen.midY - height/2,
+            width: width,
+            height: height
+        )
 
-        let styleMask: NSWindow.StyleMask = [.titled, .closable, .resizable, .miniaturizable]
-        let screenRect = NSScreen.main?.visibleFrame ?? NSRect(x:0,y:0,width:1440,height:900)
-        let w = NSRect(x: screenRect.origin.x + (screenRect.width - 1200)/2,
-                       y: screenRect.origin.y + (screenRect.height - 800)/2,
-                       width: 1200,
-                       height: 800)
-
-        window = NSWindow(contentRect: w, styleMask: styleMask, backing: .buffered, defer: false)
+        window = NSWindow(
+            contentRect: rect,
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
         window.title = "FH Browser"
-        window.contentViewController = browserVC
+        window.isReleasedWhenClosed = false
+
+        controller = BrowserViewController()
+        window.contentViewController = controller
         window.makeKeyAndOrderFront(nil)
+
+        NSApp.setActivationPolicy(.regular)
         NSApp.activate(ignoringOtherApps: true)
-
-        let mainMenu = NSMenu()
-        let appMenuItem = NSMenuItem()
-        mainMenu.addItem(appMenuItem)
-        NSApp.mainMenu = mainMenu
-
-        let appMenu = NSMenu()
-        let quitItem = NSMenuItem(title: "Quit FH Browser", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
-        appMenu.addItem(quitItem)
-        appMenuItem.submenu = appMenu
-
-        let viewMenuItem = NSMenuItem()
-        mainMenu.addItem(viewMenuItem)
-        let viewMenu = NSMenu(title: "View")
-        let toggleMobile = NSMenuItem(title: "Toggle Mobile UA", action: #selector(toggleMobile(_:)), keyEquivalent: "m")
-        toggleMobile.keyEquivalentModifierMask = [.command]
-        toggleMobile.target = self
-        viewMenu.addItem(toggleMobile)
-        viewMenuItem.submenu = viewMenu
     }
 
-    @objc func toggleMobile(_ sender: Any?) {
-        browserVC.toggleMobileMode()
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
